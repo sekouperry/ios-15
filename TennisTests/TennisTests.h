88f9596cc0e3a551f456ca951bcda16dc5d59064
= <tom.cowling@gmail.com>
2012-02-15 08:38:47 +0530

    Initial commit

---
+++ TennisTests/TennisTests.h
@@ -0,0 +1,13 @@
+//
+//  TennisTests.h
+//  TennisTests
+//
+//  Created by Tom Cowling on 14/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TennisTests : SenTestCase
+
+@end
---
+++ TennisTests/TennisTests.m
@@ -0,0 +1,32 @@
+//
+//  TennisTests.m
+//  TennisTests
+//
+//  Created by Tom Cowling on 14/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TennisTests.h"
+
+@implementation TennisTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TennisTests");
+}
+
+@end
